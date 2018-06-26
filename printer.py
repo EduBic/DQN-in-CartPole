01ddb44c06b3a0a680ade7adf90b304eddf13ab4
EduBic <bic.studio@yahoo.it>
2018-06-26 15:39:50 +0200

    refactoring printer

--- printer.py
+++ printer.py
@@ -1,21 +1,19 @@
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
 
-epoche = 50
 
+FOLDER = 'DQN/results/'
 
-def print_rewards(rewards_mat):
+def plot_rewards(files):
     plt.clf()
 
-    len1 = len(rewards_mat[0])
-    len2 = len(rewards_mat[1])
+    for nameFileCsv in files:
+        csv_file = genfromtxt(FOLDER + nameFileCsv + '.csv', delimiter=',')
+        rewards = csv_file[:, 1]
 
-    minimum = min(len1, len2)
+        steps = range(0, len(rewards))
 
-    steps = range(0, minimum)
-
-    plt.plot(steps, rewards_mat[0][:minimum], 'b', label='Batch size 64')
-    plt.plot(steps, rewards_mat[1][:minimum], 'r', label='Batch size D')
+        plt.plot(steps, rewards, label='Reward ' + nameFileCsv[:10])
 
     plt.title('Titolone')
     plt.xlabel('Step')
@@ -23,79 +21,40 @@ def print_rewards(rewards_mat):
     plt.legend()
     plt.show()
 
-
-def print_q_value(q_mat):
+def plot_q_values(files, indeces):
     plt.clf()
 
-    len1 = len(q_mat[0])
-    len2 = len(q_mat[1])
+    for nameFileCsv in files:
+        csv_file = genfromtxt(FOLDER + nameFileCsv + '.csv', delimiter=',')
+        q_value_online = csv_file[:, indeces[0]]
+        q_value_target = csv_file[:, indeces[1]]
 
-    minimum = min(len1, len2)
-    print(len(q_mat[0]))
-    steps = range(0, minimum)
+        steps = range(0, len(q_value_online))
 
-    plt.plot(steps, q_mat[0][:minimum], 'b', label='q-online')
-    plt.plot(steps, q_mat[1][:minimum], 'r', label='q-target')
+        plt.plot(steps, q_value_online, label='Q-value ' + nameFileCsv[:10])
 
-    plt.title('Titolone')
-    plt.xlabel('Episode')
-    plt.ylabel('Q-Value')
+    plt.xlabel('Step')
+    plt.ylabel('Reward')
     plt.legend()
     plt.show()
 
-
-def print_meam_epoch_q_value(mean_q_online, mean_q_target):
-    plt.clf()
-
-    x = len(mean_q_online)
-
-    epochs = range(0, x)
-
-    plt.plot(epochs, mean_q_online, 'r', label='q_online')
-    plt.plot(epochs, mean_q_target, 'b--', label='q_target')
-
-    plt.title('Mean epoch q_value')
-    plt.xlabel('Epoch')
-    plt.ylabel('Q-Value')
-    plt.legend()
-    plt.show()
 
 def main():
-    # header:
-    # step , reward
 
-    '''num_files = 2
-    rewards = [0 for i in range(num_files)]
-    q_value = [0 for i in range(num_files)]
+    files = [
+        "mse-DQN-seed-52-CartPole-v0-2018-06-26T13-56-28", 
+        "mse-DQN-seed-42-CartPole-v0-2018-06-26T10-48-11"
+    ]
 
-    nameFileCsv = "doubleDQN-2-seed-52-CartPole-v0-2018-06-24T11-55-06"
+    # step , reward, q-online, q-target
+    plot_rewards(files)
+    plot_q_values(files, indeces=[2, 3])
 
-    my_csv = genfromtxt('DQN/results/' + nameFileCsv + '.csv', delimiter=',')
-    rewards[0] = my_csv[0:, 2]
-    q_value[0] = my_csv[0:, 3]
-
-    my_csv = genfromtxt('DQN/results/' + nameFileCsv + '.csv', delimiter=',')
-    rewards[1] = my_csv[0:, 2]
-    q_value[1] = my_csv[0:, 4]
-
-    print_rewards(rewards)
-    print_q_value(q_value)'''
-
-    q_target = []
-    q_online = []
-
-
-    folder = 'csvDQN/'
-    name_file = 'ccc64.csv'
+    files_epochs = [csv_file + '-epoch' for csv_file in files]
 
     # epoch, q_online, q_target
-
-    my_csv = genfromtxt(folder + name_file, delimiter=',')
-    q_online = my_csv[:, 1]
-    q_target = my_csv[:, 2]
-
-    print_meam_epoch_q_value(q_online, q_target)
-
+    plot_q_values(files_epochs, indeces=[1, 2])
+    
 
 if __name__ == "__main__":
     main()
